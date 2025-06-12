4179c5aaa69e425efc855e10bf9974e39b8c551e
alvisjo <lwalwakk1@naver.com>
2025-06-12 15:57:27 +0900

    port 설정

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 app.register_blueprint(gemini_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render가 지정한 포트 환경변수 사용
+    app.run(host="0.0.0.0", port=port, debug=True)
